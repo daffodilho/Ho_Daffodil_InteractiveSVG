1fd86723e26efcd77a677c2c8b2a887d53c8eafb
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2019-12-02 23:01:14 -0500

    initializing app

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'How much sugar is in there?' });
+});
+
+module.exports = router;
